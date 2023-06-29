98785eed950272a38c72802b718a8cd5f42ea2e5
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2023-06-29 09:56:44 +0200

    send an html file

--- index.js
+++ index.js
@@ -17,19 +17,25 @@
 // });
 
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     // console.log('request made');
     console.log(req.url, req.method);
 
-    //set header content type
-    //what content type i want to send
+    // set header content type
     res.setHeader('Content-Type', 'text/html');
 
-    res.write('<head><link rel="stylesheet" href="#"></head>');
-    res.write('<p>hello, begiedz</p>');
-    res.write('<p>elo, begiedz</p>');
-    res.end();
+    //send an html file
+    fs.readFile('./views/index.html', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            // res.write(data);
+            res.end(data);
+        }
+    });
 });
 
 server.listen(3000, 'localhost', () => {
