7cd4e5e089c3939b6458b438ece3671699270c47
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2023-06-29 09:09:47 +0200

    Started vanilla node.js server

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-const express = require('express');
-const { readFile } = require('fs');
+// const express = require('express');
+// const { readFile } = require('fs');
 // import express from 'express';
-const app = express();
+// const app = express();
 
-app.get('/', (req, res) => {
-    readFile('./home.html', 'utf8', (err, html) => {
-        if (err) {
-            response.status(500).send('sorry, out of order');
-        }
-        res.send(html);
-    });
+// app.get('/', (req, res) => {
+//     readFile('./home.html', 'utf8', (err, html) => {
+//         if (err) {
+//             response.status(500).send('sorry, out of order');
+//         }
+//         res.send(html);
+//     });
+// });
+
+// app.listen(process.env.PORT || 3000, () => {
+//     console.log(`App avaible on http://localhost:3000`);
+// });
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log('request made');
 });
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`App avaible on http://localhost:3000`);
+server.listen(3000, 'localhost', () => {
+    console.log('listening for request on port 3000');
 });
