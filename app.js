f28f39769a307003f15cec317c47417bf215e0a4
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2023-06-29 23:10:57 +0200

    install and create ejs files

--- app.js
+++ app.js
@@ -3,26 +3,24 @@ const express = require('express');
 //express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
 //listen for req
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    // res.send('<p>home page</p>');
-    // set path to index html realtive to root
-    res.sendFile('./views/index.html', { root: __dirname });
+    res.render('index');
 });
 
 app.get('/about', (req, res) => {
-    // res.send('<p>about page</p>');
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about');
 });
 
-// redirects
-app.get('/info', (req, res) => {
-    res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+    res.render('create');
 });
 
 // 404
 app.use((req, res) => {
-    res.sendFile('./views/404.html', { root: __dirname });
+    res.status(400).render('404');
 });
