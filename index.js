b19783236bc0d07e2d317e6910393bd87ce1b1b3
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2023-06-29 09:32:07 +0200

    first response from server

--- index.js
+++ index.js
@@ -19,7 +19,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('request made');
+    // console.log('request made');
+    console.log(req.url, req.method);
+
+    //set header content type
+    //what content type i want to send
+    res.setHeader('Content-Type', 'text/plain');
+
+    res.write('hello, begiedz');
+    res.end();
 });
 
 server.listen(3000, 'localhost', () => {
