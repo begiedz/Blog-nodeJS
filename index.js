d1c9f40a440a6aceebf2ad9d1ef9d5b4e6c01f33
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2023-06-29 11:13:31 +0200

    install nodemon and lodash do i need lodash?

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
-// const express = require('express');
-// const { readFile } = require('fs');
-// import express from 'express';
-// const app = express();
-
-// app.get('/', (req, res) => {
-//     readFile('./home.html', 'utf8', (err, html) => {
-//         if (err) {
-//             response.status(500).send('sorry, out of order');
-//         }
-//         res.send(html);
-//     });
-// });
-
-// app.listen(process.env.PORT || 3000, () => {
-//     console.log(`App avaible on http://localhost:3000`);
-// });
-
 const http = require('http');
 const fs = require('fs');
+const _ = require('lodash');
 
 const server = http.createServer((req, res) => {
-    // console.log('request made');
-    console.log(req.url, req.method);
+    //lodash
+    const num = _.random(0, 20);
+    console.log(num);
 
     // set header content type
     res.setHeader('Content-Type', 'text/html');
