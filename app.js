c84bb2ebe2df08ab94a35a03695d9db630950127
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2023-06-30 10:10:41 +0200

    Add middleware

--- app.js
+++ app.js
@@ -8,11 +8,24 @@ app.set('view engine', 'ejs');
 //listen for req
 app.listen(3000);
 
+app.use((req, res, next) => {
+    console.log('new request made');
+    console.log('host: ', req.hostname);
+    console.log('path: ', req.path);
+    console.log('method: ', req.method);
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('in the next middleware');
+    next();
+});
+
 app.get('/', (req, res) => {
     const blogs = [
-        { title: 'Print dupa', snippet: ' Lorem ipsum dolor sit amet.' },
+        { title: 'Jak console.logować jak mistrz', snippet: ' Lorem ipsum dolor sit amet.' },
         { title: 'Java w wersji Script', snippet: ' Lorem ipsum dolor sit amet.' },
-        { title: 'PHHPPPP', snippet: ' Lorem ipsum dolor sit amet.' },
+        { title: 'PHP dla opornych', snippet: ' Lorem ipsum dolor sit amet.' },
     ];
     res.render('index', { title: 'Home', blogs });
 });
