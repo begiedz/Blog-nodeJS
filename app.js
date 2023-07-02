062708550d47931360c77627d9afc04f2bec0315
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2023-07-02 10:14:38 +0200

    get and save data

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+
 //express app
 const app = express();
 
 // connection to mongodb
 const dbURI =
-    'mongodb+srv://begiedz:HP7x5XUbDq9VJaiE@begiedzdb.0l1scj2.mongodb.net/begiedzdb?retryWrites=true&w=majority';
+    'mongodb+srv://begiedz:HP7x5XUbDq9VJaiE@begiedzdb.0l1scj2.mongodb.net/blogApp?retryWrites=true&w=majority';
 mongoose
     .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((result) =>
@@ -20,19 +22,45 @@ mongoose
 app.set('view engine', 'ejs');
 
 //middleware & static files
-
 app.use(express.static('public'));
-
 app.use(morgan('dev'));
-// app.use((req, res, next) => {
-//     console.log('new request made');
-//     console.log('host: ', req.hostname);
-//     console.log('path: ', req.path);
-//     console.log('method: ', req.method);
-//     next();
-// });
 
-// responses
+//mongoose and mongodb sandbox routes
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog({
+        title: 'super blog',
+        snippet: 'aaaaa',
+        body: 'lorem ipsum',
+    });
+
+    blog.save()
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => console.log(err));
+});
+
+app.get('/all-blogs', (req, res) => {
+    Blog.find()
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+app.get('/single-blog', (req, res) => {
+    Blog.findById('64a12fdabdea8358d6137f2d')
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+// routes
 app.get('/', (req, res) => {
     const blogs = [
         { title: 'Jak console.logować jak mistrz', snippet: ' Lorem ipsum dolor sit amet.' },
--- models/blog.js
+++ models/blog.js
@@ -1,4 +1,4 @@
-const { default: mongoose, mongo } = require('mongoose');
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const blogSchema = new Schema(
@@ -19,6 +19,6 @@ const blogSchema = new Schema(
     { timestamps: true }
 );
 
-const blog = mongoose.model('blog', blogSchema);
+const Blog = mongoose.model('Blog', blogSchema);
 
 module.exports = Blog;
