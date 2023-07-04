92983346c7f17070c9737e8feb88b3d0a910600d
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2023-07-04 11:15:05 +0200

    Add express router

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog');
+const blogRoutes = require('./routes/blogRoutes');
 
 //express app
 const app = express();
@@ -35,59 +35,8 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about', { title: 'About' });
 });
-
 // blog routes
-app.get('/blogs', (req, res) => {
-    Blog.find()
-        .sort({ createdAt: -1 })
-        .then((result) => {
-            res.render('index', { title: 'All blogs', blogs: result });
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
-
-app.post('/blogs', (req, res) => {
-    // req.body;
-    console.log(req.body);
-    const blog = new Blog(req.body);
-
-    blog.save()
-        .then((result) => {
-            res.redirect('/blogs');
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
-
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create a new Blog' });
-});
-
-app.get('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-
-    Blog.findById(id)
-        .then((result) => {
-            res.render('details', { blog: result, title: 'Blog Details' });
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
-app.delete('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-
-    Blog.findByIdAndDelete(id)
-        .then((result) => {
-            res.json({ redirect: '/blogs' });
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
+app.use('/blogs', blogRoutes);
 
 // 404
 app.use((req, res) => {
