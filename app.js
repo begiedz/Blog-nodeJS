58cb7617dc5cacd9d071e4c9ac89d031b8d6d027
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2023-07-20 19:27:38 +0200

    Add dotenv

--- app.js
+++ app.js
@@ -3,20 +3,22 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const blogRoutes = require('./routes/blogRoutes');
 
+require('dotenv').config();
+
 //express app
 const app = express();
 
 // connection to mongodb
-const dbURI =
-    'mongodb+srv://begiedz:HP7x5XUbDq9VJaiE@begiedzdb.0l1scj2.mongodb.net/blogApp?retryWrites=true&w=majority';
+const dbURI = process.env.DB_URI;
+
 mongoose
-    .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((result) =>
-        app.listen(3000, () => {
-            console.log(`Listening on http://localhost:3000`);
-        })
-    )
-    .catch((err) => console.log(err));
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) =>
+    app.listen(3000, () => {
+      console.log(`Listening on http://localhost:3000`);
+    })
+  )
+  .catch((err) => console.log(err));
 
 // register view engine
 app.set('view engine', 'ejs');
@@ -28,17 +30,17 @@ app.use(morgan('dev'));
 
 // routes
 app.get('/', (req, res) => {
-    res.redirect('blogs');
-    // res.render('index', { title: 'Home', blogs });
+  res.redirect('blogs');
+  // res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
-    res.render('about', { title: 'About' });
+  res.render('about', { title: 'About' });
 });
 // blog routes
 app.use('/blogs', blogRoutes);
 
 // 404
 app.use((req, res) => {
-    res.status(400).render('404', { title: '404' });
+  res.status(400).render('404', { title: '404' });
 });
